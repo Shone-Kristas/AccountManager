6fc9bb392a1cf6d4ea7951d6ca1d47babd370241
Nick Bold <kristas21@yandex.ru>
2024-06-14 00:41:49 +0200

    create test_mobile

--- AccountManagerDjango/accounts/tests/test_post_mobile.py
+++ AccountManagerDjango/accounts/tests/test_post_mobile.py
@@ -8,20 +8,19 @@ class UsersAPITestCase(TestCase):
         self.client = APIClient()
         self.url = '/api/create/'
         self.headers = {
-            'x-Device': 'mail',
+            'x-Device': 'mobile',
         }
 
     def test_create_valid_user_1(self):
         valid_payload = {
-            "name": "John",
-            "email": "john.doe@example.com",
+            "phone": "7234567890",
         }
         response = self.client.post(self.url, data=valid_payload, format='json', headers=self.headers)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_create_valid_user_2(self):
         valid_payload = {
-            "name": "John",
+            "phone": "7234567890",
             "email": "john.doe@example.com",
             "birth_date": "2000-01-01",
             "passport_number": "AB1234567",
@@ -37,7 +36,7 @@ class UsersAPITestCase(TestCase):
             "birth_date": "1995-05-15",
             "passport_number": "CD9876543",
             "birth_place": "Los Angeles",
-            "phone": "9876543210",
+            "phone": "7234567890",
             "email": "jane.smith@example.com",
             "registration_address": "789 Oak St, Los Angeles, USA",
             "residential_address": "234 Pine St, Los Angeles, USA"
@@ -45,7 +44,14 @@ class UsersAPITestCase(TestCase):
         response = self.client.post(self.url, data=another_payload, format='json', headers=self.headers)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    def test_create_validation_error_1(self):
+    def test_create_phone_error_user(self):
+        valid_payload = {
+            "phone": "8234567890",
+        }
+        response = self.client.post(self.url, data=valid_payload, format='json', headers=self.headers)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('phone', response.data)
+    def test_create_validation_error(self):
         error_payload = {
             "name": "John",
             "birth_date": "2000-01-01",
@@ -54,25 +60,14 @@ class UsersAPITestCase(TestCase):
         response = self.client.post(self.url, data=error_payload, format='json', headers=self.headers)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("error", response.data)
-        self.assertIn("Username and email are required for mail device.", response.data["error"])
-
-    def test_create_validation_error_2(self):
-        error_payload = {
-            "email": "john.doe@example.com",
-            "birth_date": "2000-01-01",
-        }
-        response = self.client.post(self.url, data=error_payload, format='json', headers=self.headers)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertIn("error", response.data)
-        self.assertIn("Username and email are required for mail device.", response.data["error"])
+        self.assertIn("Phone number is required for mobile device.", response.data["error"])
 
     def test_unsupported_device(self):
         headers = {
             'x-Device': 'unsupported',
         }
         valid_payload = {
-            "name": "John",
-            "email": "john.doe@example.com",
+            "phone": "7234567890",
         }
         response = self.client.post(self.url, data=valid_payload, format='json', headers=headers)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
